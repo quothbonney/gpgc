e2f3f1f88eadeac1bba301f95141c4b336592739
quothbonney <jackie.d.carson@gmail.com>
2023-01-02 23:24:46 -0600

    Fixed to gitignore

--- py/decode.py
+++ py/decode.py
@@ -6,6 +6,7 @@ import math
 
 if __name__ == '__main__':
     data = []
+    vectors = []
 
     b = [0]
     x0 = [0]
@@ -13,13 +14,15 @@ if __name__ == '__main__':
 
     with open("output.txt") as gpgc:
         for line in gpgc:
-            words = line.split(" ");
+            words = line.split(" ")
             #data.append((int(words[3]), (float(words[0]), float(words[1]), int(words[2]))));
             max_size = 512;
             size = int(words[3])
             exp = max_size / size
             data.append(int(math.log2(exp)))
-    print(data)
+            vectors.append((float(words[0]), float(words[1]), float(words[2]), int(size)))
+
+        
     index = 0
     while index < len(data):
         while b[index] < data[index]:
@@ -38,6 +41,25 @@ if __name__ == '__main__':
 
         index += 1
 
-    print(x0)
-    print(y0)
+    #print(x0)
+    #print(y0)
+
+    decompressed = np.zeros((512, 512), dtype=int)
+
+    for elem in range(len(x0)):
+        x_o = int(x0[elem] * 512)
+        y_o = int(y0[elem] * 512)
+        #print(x_o, y_o)
+        
+        i, j, k, size = vectors[elem]
+        for m in range(size):
+            for n in range(size):
+                altitude = (i * m) + (j * n) + k
+                decompressed[y_o + m][x_o + n] = int(altitude)
+
+
+    plt.imshow(decompressed, cmap='hot')
+    plt.show()
+
+
 
